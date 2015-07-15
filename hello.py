d8a454e903714fbbe4651f9a27627ba098345ae0
Andrey <andreinik@gmail.com>
2015-07-15 08:54:02 -0400

    New version

--- hello.py
+++ hello.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
 BLUE = "#0099FF"
 GREEN = "#33CC33"
-
+counter=0
 COLOR = BLUE
 
 @app.route('/')
 def hello():
-
+	global counter
+	counter +=1
 	return """
 	<html>
 	<body bgcolor="{}">
@@ -18,10 +19,11 @@ def hello():
 	<center><h1><font color="white">Hi, I'm GUID:<br/>
 	{}
 	</center>
+	<center>{}</center>
 
 	</body>
 	</html>
-	""".format(COLOR,my_uuid)
+	""".format(COLOR,my_uuid,counter)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
